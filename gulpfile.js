20ee71abb2a1c517f61b9d065d2688f33db7b0fe
Jan Schneider <schneider@zeiss-izm.de>
2017-09-28 11:46:46 +0200

    reference

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var concat = require("gulp-concat");
 var hljs = require('highlight.js');
 var gulp_markdown = require('gulp-markdown-it');
-
+var typedoc = require("gulp-typedoc");
 var concat = require("gulp-concat");
 
 const paths = {
@@ -46,6 +46,24 @@ gulp.task('compile', function () {
         }));
 });
 
-gulp.task('run', ['copy','compile'], function () {
+gulp.task("typedoc", function() {
+    return gulp
+        .src(["../../../Utilities/Zeiss/IMT/JavaScript/CustomPlotEngine/Api/Sources"])
+        .pipe(typedoc({
+            ignoreCompilerErrors: true,
+            module: "commonjs",
+            mode:"modules",
+            target: "ES6",
+            out: "reference/",
+            name: "PiWeb Customplot API Reference",
+            excludeExternals: true,
+            excludePrivate: true,
+            includeDeclarations:false,
+            exclude: "**/internal/**",
+            logger: "none"           
+        }));
+});
+
+gulp.task('run', ['copy','compile','typedoc'], function () {
     gulp.watch(['**/*.md', '**/*.css'], ['copy','compile']);
 });
